149859dd22aba5449b5b94cbc7012f6df0f970e9
Florian Goujeon <florian.goujeon@gmail.com>
2018-08-16 20:10:26 +0200

    Draw a red rectangle at the center of the window

---
+++ app/src/main.cpp
@@ -0,0 +1,142 @@
+#include <SDL2/SDL.h>
+
+#ifdef EMSCRIPTEN
+#include <emscripten/html5.h>
+#include <emscripten.h>
+#endif
+
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+
+struct context
+{
+    SDL_Window* pwindow = nullptr;
+    SDL_Renderer* prenderer = nullptr;
+    bool quit = false;
+};
+
+void process_events(context& ctx)
+{
+    SDL_Event event;
+    while(SDL_PollEvent(&event))
+    {
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                ctx.quit = true;
+                break;
+        }
+    }
+}
+
+void iterate(void *arg)
+{
+    auto* pctx = static_cast<context*>(arg);
+    auto& ctx = *pctx;
+
+    process_events(ctx);
+
+    SDL_Renderer* prenderer = ctx.prenderer;
+
+    int window_width;
+    int window_height;
+    SDL_GetWindowSize(ctx.pwindow, &window_width, &window_height);
+
+    //white background
+    SDL_SetRenderDrawColor(prenderer, 255, 255, 255, 255);
+    SDL_RenderClear(prenderer);
+
+    //centered red rectangle
+    {
+        const auto size = 50;
+
+        SDL_Rect r;
+        r.x = window_width / 2 - size / 2;
+        r.y = window_height / 2 - size / 2;
+        r.w = size;
+        r.h = size;
+        SDL_SetRenderDrawColor(prenderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(prenderer, &r);
+    }
+
+    SDL_RenderPresent(prenderer);
+}
+
+#ifdef EMSCRIPTEN
+void sync_window_size_with_canvas_size(context& ctx)
+{
+    double width, height;
+    emscripten_get_element_css_size("canvas", &width, &height);
+    SDL_SetWindowSize(ctx.pwindow, width, height);
+}
+
+int on_canvas_resize(int, const EmscriptenUiEvent*, void* arg)
+{
+    auto* pctx = static_cast<context*>(arg);
+    auto& ctx = *pctx;
+    sync_window_size_with_canvas_size(ctx);
+    return 0;
+}
+
+void run(context& ctx)
+{
+    sync_window_size_with_canvas_size(ctx);
+    emscripten_set_resize_callback(nullptr, &ctx, false, on_canvas_resize);
+    emscripten_set_main_loop_arg
+    (
+        iterate,
+        &ctx,
+        -1, // call the function as fast as the browser wants to render (typically 60fps)
+        1 //call the function repeatedly
+    );
+}
+#else
+void run(context& ctx)
+{
+    const unsigned int frame_duration = 1000 / 60;
+
+    while(!ctx.quit)
+    {
+        const auto begin_time = SDL_GetTicks();
+        iterate(&ctx);
+        const auto end_time = SDL_GetTicks();
+
+        //regulate the frame rate
+        assert(end_time >= begin_time);
+        const auto elapsed_time = end_time - begin_time;
+        if(elapsed_time < frame_duration)
+        {
+            const Uint32 remaining_time = frame_duration - elapsed_time;
+            SDL_Delay(remaining_time);
+        }
+    }
+}
+#endif
+
+int main(int, char**)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* pwindow = SDL_CreateWindow
+    (
+        "Test",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_RESIZABLE
+    );
+    SDL_Renderer* prenderer = SDL_CreateRenderer(pwindow, -1, 0);
+
+    context ctx;
+    ctx.pwindow = pwindow;
+    ctx.prenderer = prenderer;
+
+    run(ctx);
+
+    SDL_DestroyRenderer(prenderer);
+    SDL_DestroyWindow(pwindow);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
