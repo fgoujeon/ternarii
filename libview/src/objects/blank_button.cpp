79d0a7bc8f1238c68c65b0d2cc196d213a04cf98
Florian Goujeon <florian.goujeon@gmail.com>
2020-06-01 11:47:46 +0200

    libview: Add blank_button

---
+++ libview/src/objects/blank_button.cpp
@@ -0,0 +1,92 @@
+/*
+Copyright 2018 - 2020 Florian Goujeon
+
+This file is part of Ternarii.
+
+Ternarii is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Ternarii is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Ternarii.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "blank_button.hpp"
+#include "../colors.hpp"
+#include <libres.hpp>
+
+namespace libview::objects
+{
+
+blank_button::blank_button
+(
+    Object2D& parent,
+    features::drawable_group& drawables,
+    features::clickable_group& clickables,
+    const style& stl,
+    const callback_set& callbacks
+):
+    Object2D{&parent},
+    features::clickable{*this, &clickables},
+    style_(stl),
+    callbacks_(callbacks),
+    background_rectangle_
+    (
+        *this,
+        drawables,
+        rounded_rectangle::style
+        {
+            .color = style_.color,
+            .radius = style_.radius
+        }
+    )
+{
+    background_rectangle_.scale({1.0f, 0.2f});
+}
+
+void blank_button::set_enabled(const bool enabled)
+{
+    enabled_ = enabled;
+}
+
+bool blank_button::is_inside(const Magnum::Vector2& model_space_position) const
+{
+    if(!enabled_)
+    {
+        return false;
+    }
+
+    const auto x = model_space_position.x();
+    const auto y = model_space_position.y();
+    return -1 <= x && x <= 1 && -0.2 <= y && y <= 0.2;
+}
+
+void blank_button::handle_mouse_press()
+{
+    background_rectangle_.set_color(style_.highlight_color);
+    callbacks_.mouse_press_callback();
+}
+
+void blank_button::handle_mouse_release()
+{
+    background_rectangle_.set_color(style_.color);
+    callbacks_.mouse_release_callback();
+}
+
+void blank_button::handle_mouse_enter()
+{
+    background_rectangle_.set_color(style_.highlight_color);
+}
+
+void blank_button::handle_mouse_leave()
+{
+    background_rectangle_.set_color(style_.color);
+}
+
+} //namespace
---
+++ libview/src/objects/blank_button.hpp
@@ -0,0 +1,81 @@
+/*
+Copyright 2018 - 2020 Florian Goujeon
+
+This file is part of Ternarii.
+
+Ternarii is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Ternarii is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Ternarii.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBVIEW_OBJECTS_BLANK_BUTTON_HPP
+#define LIBVIEW_OBJECTS_BLANK_BUTTON_HPP
+
+#include "label.hpp"
+#include "rounded_rectangle.hpp"
+#include "../common.hpp"
+#include <libutil/void_function.hpp>
+
+namespace libview::objects
+{
+
+class blank_button: public Object2D, public features::clickable
+{
+    public:
+        struct callback_set
+        {
+            libutil::void_function<> mouse_press_callback;
+            libutil::void_function<> mouse_release_callback;
+        };
+
+        struct style
+        {
+            Magnum::Color4 color;
+            Magnum::Color4 highlight_color;
+            Magnum::Color4 outline_color;
+            float radius = 0;
+        };
+
+    public:
+        blank_button
+        (
+            Object2D& parent,
+            features::drawable_group& drawables,
+            features::clickable_group& clickables,
+            const style& stl,
+            const callback_set& callbacks
+        );
+
+        void set_enabled(const bool enabled);
+
+    //features::clickable virtual functions
+    private:
+        bool is_inside(const Magnum::Vector2& model_space_position) const override;
+
+        void handle_mouse_press() override;
+
+        void handle_mouse_release() override;
+
+        void handle_mouse_enter() override;
+
+        void handle_mouse_leave() override;
+
+    private:
+        style style_;
+        const callback_set callbacks_;
+        rounded_rectangle background_rectangle_;
+        bool enabled_ = true;
+};
+
+} //namespace
+
+#endif
