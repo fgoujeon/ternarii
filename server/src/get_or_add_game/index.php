a209779f51f240886e309b59d42ab933947b7d51
Florian Goujeon <florian.goujeon@gmail.com>
2021-09-10 09:13:08 +0200

    Replace add_game with get_or_add_game

--- server/src/get_or_add_game/index.php
+++ server/src/get_or_add_game/index.php
@@ -31,6 +31,16 @@ if (!password_verify($player_password, $player_password_hash)) {
     exit("Incorrect password");
 }
 
+#get unfinished game, if any
+$query = sprintf(
+    "SELECT `id` FROM `unverified_game` WHERE `player_id` = '%s' AND `stage_id` = '%s' AND `is_over` = FALSE LIMIT 1",
+    $mysqli->real_escape_string($player_id),
+    $mysqli->real_escape_string($stage_id));
+$result = $mysqli->query("$query");
+if ($row = $result->fetch_assoc()) {
+    exit($row["id"]);
+}
+
 #create game
 $query = sprintf(
     "INSERT INTO `unverified_game` (`id`, `player_id`, `stage_id`, `first_input_random_number`, `first_next_input_random_number`, `is_over`) " .
@@ -38,8 +48,7 @@ $query = sprintf(
     $mysqli->real_escape_string($player_id),
     $mysqli->real_escape_string($stage_id),
     rand(),
-    rand()
-);
+    rand());
 $result = $mysqli->query("$query");
 
 #return game ID
