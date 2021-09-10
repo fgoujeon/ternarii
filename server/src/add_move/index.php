22a0c78825f6a09a77d03964fac8f169f9b90c7a
Florian Goujeon <florian.goujeon@gmail.com>
2021-09-10 15:50:58 +0200

    Rename move_id into move_idx to avoid confusion (it isn't a primary key)

--- server/src/add_move/index.php
+++ server/src/add_move/index.php
@@ -6,7 +6,7 @@ try {
     $player_id = $_GET["player_id"];
     $player_password = $_GET["player_password"];
     $game_id = $_GET["game_id"];
-    $move_id = $_GET["move_id"];
+    $move_idx = $_GET["move_idx"];
     $column_offset = $_GET["column_offset"];
     $rotation = $_GET["rotation"];
 
@@ -29,29 +29,29 @@ try {
 
     #get expected move ID
     $query = sprintf(
-        "SELECT MAX(`id`) as `max_id` FROM `unverified_game_move` WHERE `game_id` = '%s'",
+        "SELECT MAX(`idx`) as `max_idx` FROM `unverified_game_move` WHERE `game_id` = '%s'",
         $mysqli->real_escape_string($game_id),
     );
     $result = $mysqli->query("$query");
     if ($row = $result->fetch_assoc()) {
-        $expected_move_id = (int)$row["max_id"] + 1;
+        $expected_move_idx = (int)$row["max_idx"] + 1;
     } else {
-        $expected_move_id = 0;
+        $expected_move_idx = 0;
     }
 
     #check move ID
-    if ($move_id != $expected_move_id)
+    if ($move_idx != $expected_move_idx)
     {
-        throw new Exception("Unexpected move ID. Expected " . $expected_move_id . ", got " . $move_id);
+        throw new Exception("Unexpected move index. Expected " . $expected_move_idx . ", got " . $move_idx);
     }
 
     #insert new move
     $next_input_random_number = rand();
     $query = sprintf(
-        "INSERT INTO `unverified_game_move` (`game_id`, `id`, `time`, `column_offset`, `rotation`, `next_input_random_number`) " .
+        "INSERT INTO `unverified_game_move` (`game_id`, `idx`, `time`, `column_offset`, `rotation`, `next_input_random_number`) " .
         "VALUES ('%s', '%s', CURRENT_TIMESTAMP, b'%s', b'%s', '%s');",
         $mysqli->real_escape_string($game_id),
-        $mysqli->real_escape_string($move_id),
+        $mysqli->real_escape_string($move_idx),
         $mysqli->real_escape_string(decbin($column_offset)),
         $mysqli->real_escape_string(decbin($rotation)),
         $mysqli->real_escape_string($next_input_random_number));
