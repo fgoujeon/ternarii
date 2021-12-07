aec1e547fb28dead810bdd46346cbae5d73d3fb1
Florian Goujeon <florian.goujeon@gmail.com>
2021-12-07 20:27:54 +0100

    Reduce the amount of negative adder tiles

--- libgame/src/input_generators.cpp
+++ libgame/src/input_generators.cpp
@@ -392,10 +392,10 @@ namespace
         (
             {
                 {get_random_number_tile_pair_generator(), 5000},
-                {get_adder_generator<-2>(),               50},
-                {get_adder_generator<-1>(),               50},
-                {get_adder_generator<1>(),                50},
-                {get_adder_generator<2>(),                50}
+                {get_adder_generator<-2>(),               30},
+                {get_adder_generator<-1>(),               30},
+                {get_adder_generator<1>(),                70},
+                {get_adder_generator<2>(),                70}
             }
         );
         return generator;
